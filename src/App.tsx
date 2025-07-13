17a8f9c2549003b89dfcc9414804bdd8ef1d608f
rahulk31 <rahulsingh3118.rs@gmail.com>
2025-07-14 01:36:14 +0530

    :sparkle:[feat]: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      AI Resume Builder
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
